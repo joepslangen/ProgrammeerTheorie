6af31865e1f930e93f9d7fbd2652a719601a8cd0
Jasper <jasper@DESKTOP-Q15T09F.localdomain>
2022-01-12 14:46:55 +0100

    removed unused functions

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ from car import Car
 Rushhour_df = pd.read_csv("gameboards/Rushhour6x6_1.csv")
 
 game = Board(Rushhour_df)
-game.load_cars(Rushhour_df), 
-game.place_car()
+game.load_cars(Rushhour_df)
 game.printBoard()
-game.moveCheck()
-game.moveCarLeft("A")
-game.place_car()
+game.moveCarLeft("C")
 game.printBoard()
-game.moveCheck()
