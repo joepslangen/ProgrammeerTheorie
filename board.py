a265a44c259d6d371fa0baa3d83f92446520c419
Jasper <jasper@DESKTOP-Q15T09F.localdomain>
2022-01-12 11:12:30 +0100

    addition of movement functions

--- board.py
+++ board.py
@@ -21,16 +21,18 @@ class Board():
         #to provide a barrier around the drivable gameboard 
         #this was needed to prevent out of range errors ocurring in the moveCheck function
         ones_matrix = np.ones((self.dimensions + 2, self.dimensions + 2))
-
         #actual drivable gameboard consists of zeros with dimensions: dimension x dimension
         zeros_matrix = np.zeros((self.dimensions, self.dimensions))
-
         #combine the ones matrix and the zeros matrix to create empty board with barriers
         ones_matrix[1:self.dimensions + 1, 1:self.dimensions + 1] = zeros_matrix
+        #define a clean board with barrier with the correct dimensions
+        self.clean_board = ones_matrix
+        #set the game board to the clean board 
+        self.game_board = pd.DataFrame(self.clean_board).astype(int)
 
-        #use pandas to create 6x6 DataFrame from the board matrix matrix
-        self.game_board = pd.DataFrame(ones_matrix).astype(int)
-
+    #load cars from csv file into self.cars
+    #self.cars car properties will be changed by moving the cars
+    #this loop will only run once
     def load_cars(self, Rushhour_df):
         # cycle through every row in the dataframe
         for index in range(len(Rushhour_df)):
@@ -44,14 +46,15 @@ class Board():
             car_row = Rushhour_df.loc[index, "row"]
             #extract lenght from column "length"
             car_length = Rushhour_df.loc[index, "length"]
-            
             #define car using Car() class and extracted properties
             car = Car(car_name, car_orientation, car_column, car_row, car_length)
-
             #add car to cars list
             self.cars.append(car)
 
+    #place cars from self.cars onto a clean game board
     def place_car(self):
+        #clear the previous game board before placing cars in new positions
+        self.game_board = pd.DataFrame(self.clean_board).astype(int)
         #cycle through the cars list
         for car in self.cars: 
             #check if car is horizontal
@@ -59,18 +62,13 @@ class Board():
                 #cycle through lenght of the car
                 for index in range(0, car._length):
                     #replace zeros in DataFrame with the carname in horizontal fashion
-                    #correction of 1 since we added a 1 thick layer of barrier around the board
                     self.game_board.loc[car.row, car.column + index] = car._name
             #if not horizontal, car is vertical
             else:
                 #cycle through lenght of the car
                 for index in range(0, car._length):
                     #replace zeros in DataFrame with the carname in vertical fashion
-                    #correction of 1 since we added a 1 thick layer of barrier around the board
-                    self.game_board.loc[car.row + index, car.column] = car._name
-    
-        #show gameboard
-        print(self.game_board)
+                    self.game_board.loc[car.row + index, car.column] = car._name.lower()
 
     #start of a function able to move cars
     def moveCheck(self):
@@ -89,3 +87,33 @@ class Board():
                     print("The car:", car._name, "can move up")
                 if self.game_board.loc[car.row + car._length, car.column] == 0:
                     print("The car:", car._name, "can move down")
+    
+    #to move car left, change its column position
+    def moveCarLeft(self, carname):
+        for car in self.cars: 
+            if car._name == carname: 
+                car.column -= 1
+
+    #to move car right, change its column position
+    def moveCarRight (self, carname):
+        for car in self.cars: 
+            if car._name == carname: 
+                car.column += 1
+    
+    #to move car up, change its row position
+    def moveCarUp (self, carname):
+        for car in self.cars: 
+            if car._name == carname: 
+                car.row -= 1
+    
+    #to move car down, change its row position
+    def moveCarDown(self, carname):
+        for car in self.cars: 
+            if car._name == carname: 
+                car.row += 1
+
+    #function to print game board
+    def printBoard(self):
+        print(self.game_board)
+    
+    
