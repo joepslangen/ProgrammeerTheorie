0cf988d5fe538c2fd6e68385c23200d8c76798b4
Eva <embiesot@hotmail.com>
2022-01-17 11:09:42 +0100

    Fix moving cars, cars cannot move through each other anymore. Add move counter

--- test_pygame.py
+++ test_pygame.py
@@ -71,6 +71,9 @@ pink_car = Car(255, 180, 52, 120)
 # Game Loop
 running = True
 
+# Move counter
+counter = 0
+
 while running:
     # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
     pygame.time.delay(100) 
@@ -79,7 +82,7 @@ while running:
         # Checks if the red button in the corner of the window is clicked
         if event.type == pygame.QUIT: 
             # Ends the game loop
-            run = False 
+            running = False 
     # This will give us a dictonary where each key has a value of 1 or 0. Where 1 is pressed and 0 is not pressed 
     keys = pygame.key.get_pressed()  
 
@@ -110,54 +113,75 @@ while running:
     if keys[pygame.K_DOWN] and green_car.y < 500 - green_car.height - green_car.movement_speed:
         green_car.y += green_car.movement_speed
 
-    # if mouse is pressed
+   # When mouse is pressed
     if pygame.mouse.get_pressed()[0]:
         # When mouse is pressed on the red car
         if (pygame.mouse.get_pos()[0] > red_car.x) & (pygame.mouse.get_pos()[0] < red_car.x + red_car.width) & (pygame.mouse.get_pos()[1] > red_car.y) & (pygame.mouse.get_pos()[1] < red_car.y + red_car.height):
             # When mouse is pressed on the right side of the car
             if pygame.mouse.get_pos()[0] > red_car.x + (red_car.width / 2):
-                print(((pink_car.y + pink_car.height) > red_car.y > pink_car.y))
-                # if the pink car blocks the red car, don't move
+                # If the pink car blocks the red car, don't move
                 if (0 < (pink_car.x - red_car.x - red_car.width) < 60) and ((pink_car.y + pink_car.height) >= red_car.y >= pink_car.y):
                     continue
-                # if the green car does blocks the red car, don't move
+                # If the green car does blocks the red car, don't move
                 elif ((green_car.x - red_car.x - red_car.width) < 60) and ((green_car.y + green_car.height) >= red_car.y >= green_car.y):
                     continue
-                # if not blocked, move to the right
+                # If not blocked, move to the right
                 else:
                     red_car.x += 70
+                    counter += 1
             # When mouse is pressed on left side of the car
             else:
-                # if the pink car blocks the red car, don't move
+                # If the pink car blocks the red car, don't move
                 if (0 < (red_car.x - (pink_car.x + pink_car.width)) < 60) and ((pink_car.y + pink_car.height) >= red_car.y >= pink_car.y):
                     continue
-                # if the green car does blocks the red car, don't move
+                # If the green car does blocks the red car, don't move
                 elif (0 < (red_car.x - (green_car.x + green_car.width)) < 60) and ((green_car.y + green_car.height) >= red_car.y >= green_car.y):
                     continue
-                # if not blocked, move to the left
+                # If not blocked, move to the left
                 else:
                     red_car.x -= 70
-    # if mouse is pressed
-    if pygame.mouse.get_pressed()[0]:
+                    counter += 1
+
         # When mouse is pressed on the green car
         if (pygame.mouse.get_pos()[0] > green_car.x) & (pygame.mouse.get_pos()[0] < green_car.x + green_car.width) & (pygame.mouse.get_pos()[1] > green_car.y) & (pygame.mouse.get_pos()[1] < green_car.y + green_car.height):
-            # When mouse is pressed on the upper half of the car, move up
-            if pygame.mouse.get_pos()[1] > green_car.y + (green_car.height / 2):
-                green_car.y += 63
             # When mouse is pressed on the lower half of the car, move down
+            if pygame.mouse.get_pos()[1] > green_car.y + (green_car.height / 2):
+                # If the red car blocks the green car, don't move
+                if (0 < (red_car.y - (green_car.y + green_car.height)) < 60) and ((red_car.x + red_car.width) >= (green_car.x + green_car.width / 2) >= red_car.x):
+                    continue
+                # If not blocked, move down
+                else:
+                    green_car.y += 63
+                    counter += 1
+            # When mouse is pressed on the upper half of the car, move up
             else: 
-                green_car.y -= 63
-    # if mouse is pressed
-    if pygame.mouse.get_pressed()[0]:
+                # If the red car blocks te green car, don't move
+                if (0 < (green_car.y - red_car.y) < 60) and ((red_car.x + red_car.width) >= (green_car.x + green_car.width / 2) >= red_car.x):
+                    continue
+                # If not blocked, move up
+                else:
+                    green_car.y -= 63
+                    counter += 1
         # When mouse is pressed on the pink car
         if (pygame.mouse.get_pos()[0] > pink_car.x) & (pygame.mouse.get_pos()[0] < pink_car.x + pink_car.width) & (pygame.mouse.get_pos()[1] > pink_car.y) & (pygame.mouse.get_pos()[1] < pink_car.y + pink_car.height):
-             # When mouse is pressed on the upper half of the car, move up
-            if pygame.mouse.get_pos()[1] > pink_car.y + (pink_car.height / 2):
-                pink_car.y += 60
              # When mouse is pressed on the lower half of the car, move down
+            if pygame.mouse.get_pos()[1] > pink_car.y + (pink_car.height / 2):
+                # If the red car blocks the pink car, don't move
+                if (0 < (red_car.y - (pink_car.y + pink_car.height)) < 60) and ((red_car.x + red_car.width) >= (pink_car.x + pink_car.width / 2) >= red_car.x):
+                    continue
+                # If not blocked, move down
+                else:
+                    pink_car.y += 60
+                    counter += 1
+             # When mouse is pressed on the upper half of the car, move up
             else: 
-                pink_car.y -= 60
-
+                # If the red car blocks the pink car, don't move
+                if (0 < (pink_car.y - red_car.y) < 60) and ((red_car.x + red_car.width) >= (pink_car.x + pink_car.width / 2) >= red_car.x):
+                    continue
+                # If not blocked, move up
+                else:
+                    pink_car.y -= 60
+                    counter += 1
     # Call blit method to draw image on screen
     win.blit(picture, (0, 0))
 
