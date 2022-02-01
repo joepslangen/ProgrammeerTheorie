53d3d28063589e2f37db4358084cd135f477f8b0
Jasper <jasper@DESKTOP-Q15T09F.localdomain>
2022-02-01 14:46:38 +0100

    added correct comments

--- code/algorithms/BFS_plus_prune.py
+++ code/algorithms/BFS_plus_prune.py
@@ -4,92 +4,84 @@ import timeit
 
 
 class BFS_plus_prune(): 
-    """
-    Define function for breadth first algorithm. 
-    """
+    """The BFS_plus class containing the Breadth First Search + Heuristics (BFS+) algorithm."""
     def __init__(self, board): 
         self.board = board
         self.start, self.stop = 0, 0
     
-    """
-    Define function for breadth first algorithm with heuristics
-    """
+
     def bfs_plus_prune(self):
+        """The BFS algorithm. 
+
+        The BFS algorithm searches a tree datastructure. It starts at the tree root 
+        and explores all nodes at the present depth before moving on to the nodes
+        at the next depth level, unless a node shows potential. In this scenario the 
+        children of this node will be placed at the front of the queue, resulting in a 
+        breadth first + depth first hybrid. It creates a queue, checks the first node in the queue
+        and appends all "good" children nodes to the front of the queue. Might not find optimal solution. 
         """
-        Creation of the BFS queue moves and required variables containing the path, 
-        the next possible states and the start configuration of the board. 
-        """
+        # initialize queue, path and start position
         moves = deque()
         moves.appendleft("")
         path = ""
         startposion = self.board.noprintBoard()
 
+        # start algorithm timer
         self.start = timeit.default_timer()
         while self.board.running == True:
-            """
-            Place cars in initial configuration
-            """
+            # place cars in initial configuration
             self.place_car_init()
 
-            """
-            Take the first state from the queue and print
-            path length. 
-            """
+            # take first path from the queue
             path = moves.popleft()
 
-            """
-            Place cars in configuration as states in the path
-            """
+            # place cars corresponding to chosen path
             self.place_car_current(path)
             
-            """
-            Check if the currently loaded gameboard has previously been loaded,
-            excluding the start configuration. Continue and add to history or skip
-            this board.
-            """
+            # check if currently loaded gameboard has previously been loaded, excluding start config. Skip if necessary 
             if self.board.noprintBoard() in self.board.history and self.board.noprintBoard() != startposion: 
                 continue
             else: 
                 self.board.history.append(self.board.noprintBoard())
 
-            """
-            Check which car can move in which directions 
-            and keep the info in a list
-            """
+            # check movable directions for each car and keep them in a list
+            # these are the children of the current path
             moving_cars = self.check_car_movement()
 
-            """
-            Go through with the BFS algorithm while applying heuristics
-            """
-            print(path)
-            print(len(path)/2)
-            x = False
+            # check if red car moved in 200 steps
+            redmove = False
             if len(path) == 200:
-                for j in range(0, len(path), 2):
-                    if path[j] == "X":
-                        x = True
-                if not x:
+                for carname in range(0, len(path), 2):
+                    # cycle through the path and check 
+                    # if the red car has moved in the first 200 steps
+                    if path[carname] == "X":
+                        redmove = True
+                # if the red car didn't move in 200 steps abandon this path
+                if not redmove:
                     continue
+            # if path is longer than 500 moves, abandon path
             elif len(path) > 500:
                 continue
-            # else:
+
+            # go through with the BFS algorithm while applying heuristics
             moves = self.heuristics(moving_cars, path, moves)
+        
+        # stop algorithm timer
         self.stop = timeit.default_timer()
+    
+        # return runtime and found path length
         return self.stop - self.start, len(path)/2
 
     def place_car_init(self): 
-        """
-        Set the cars to the original start configuration and create
-        a deep copy of this state. 
-        """
+        """Sets the cars to the original start configuration."""
         self.board.place_car(self.board._cars_init)
         self.board.cars = pickle.loads(pickle.dumps(self.board._cars_init))
     
     def place_car_current(self, path): 
-        """
-        Read through the chosen path. 
-        Determine car name and which direction to move in. 
-        Move car in correct directions to update game board. 
+        """Sets the cars to the configuration as described by the path
+        
+        Read through the path, determines which car should move in 
+        what direction and move car accordingly. 
         """
         for i in range(0, len(path), 2):
             if path[i + 1] == 'L':
@@ -102,7 +94,8 @@ class BFS_plus_prune():
                 self.board.moveCarDown(path[i])
     
     def check_car_movement(self): 
-        """
+        """Check in which directions cars can move. 
+
         Move through the cars in self.board.cars. 
         Check if they can move left, right, up or down
         and add their name + direction to moving_cars list. 
@@ -111,25 +104,30 @@ class BFS_plus_prune():
         moving_cars = []
         for car in self.board.cars:
             if car._orientation == "V":  
+                # if the cell below car is empty, car can move down
                 if self.board.gameboard[car.row + car._length][car.column] == "0":
                     moving_cars.append(car._name)
                     moving_cars.append('D')
+                # if cell above car is empty, car can move up
                 if self.board.gameboard[car.row - 1][car.column] == "0":
                     moving_cars.append(car._name)
                     moving_cars.append('U')
             if car._orientation == "H":  
+                # if cell on right side of car is empty, car can move right
                 if self.board.gameboard[car.row][car.column + car._length] == "0":
                     moving_cars.append(car._name)
                     moving_cars.append('R')
+                # if cell on left side of car is empty, car can move left
                 if self.board.gameboard[car.row][car.column - 1] == "0":
                     moving_cars.append(car._name)
                     moving_cars.append('L')
         return moving_cars
 
     def heuristics(self, moving_cars, path, moves): 
-        """
-        Simple introduction of heuristics. Determine if the Red car
-        could move to the right. If so, this move is brings us 
+        """Simple introduction of heuristics by introducing priority-children.
+
+
+        Determine if the Red car could move to the right. If so, this move is brings us 
         closer to the endgame and thus this configuration will 
         have priority over the others and will be placed in the prio1 queue. 
         Moving cars to the left creates space on the right of the Red car, 
@@ -144,34 +142,34 @@ class BFS_plus_prune():
         prio1 = deque("")
         prio2 = deque("")
         prio3 = deque("")
-
         for i in range(0, len(moving_cars), 2):
             move = moving_cars[i] + moving_cars[i + 1]
             put = path + move
+            # if red car can move right, add to prio1
             if moving_cars[i] == "X" and moving_cars[i + 1] == "R": 
                 prio1.appendleft(put)
+            # if other horizontal cars can move left, add to prio2
             elif moving_cars[i + 1] == "L" and moving_cars[i] != "X": 
                 prio2.appendleft(put)
+            # if vertical cars can move up or down, add to prio3
             elif moving_cars[i + 1] == "U" or moving_cars[i + 1] == "D": 
                 prio3.appendleft(put)
             else:
                 moves.append(put)
                 
             self.check_endgame(move, path)
-        """
-        Combine the priority queues with the normal queue 
-        to feed to the next cycle of the bfs algorithm. 
-        """
+        
+        # combine priority queues into single queue and return
         moves = prio1 + prio2 + prio3 + moves
         return moves
 
     def check_endgame(self, move, path): 
-        """
-        Again a end-game check function. 
-        """
+        """Check if game has ended."""
         if move == "XR":
             self.board.moveCarRight('X')
+            # set car variable to the red car which is always the last in the cars list
             car = self.board.cars[-1]
+            # check if cell to the right of red car is the exit and end game if needed
             if self.board.gameboard[car.row][car.column + car._length] == "2":
                 print("Path:", path + move)
                 self.board.stop = timeit.default_timer()
