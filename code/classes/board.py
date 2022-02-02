95b2a8377fa96fe08cf0ceb6c2aec35dee3ce956
Jasper <jasper@DESKTOP-Q15T09F.localdomain>
2022-02-02 12:13:41 +0100

    updated comments

--- code/classes/board.py
+++ code/classes/board.py
@@ -1,6 +1,3 @@
-"""
-Required imports
-"""
 import pandas as pd
 import numpy as np
 import sys
@@ -11,46 +8,30 @@ import timeit
 import os
 import pickle
 
-"""
-Creation of the Board class. This class does everything 
-regarding the gameboard. It sets it up, moves cars
-prints and clears the board. 
-"""
 class Board():
-
+    """The Board class, containing everything related to the board"""
     def __init__(self, dimensions):
-        """
-        self.history required for keeping track of boards previously seen 
-        self.running to check if game has ended
-        self.start/stop/movecounter to keep track of interesting variables
-        """
+        # history, gamestatus, timer and move counter variables
         self.history = []
         self.running = True
         self.start, self.stop, self.movecounter = timeit.default_timer(), 0, 0
 
-        """
-        Colour list and dictionary. Required for colour printing cars. 
-        """
+        # list and dictionary of colours, required for colour printing in terminal
         self.colour_list = ['green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
         self.colourdict = {}
 
-        """
-        Car list to contain cars. Initial row and column dictionaries to 
-        keep track of cars' starting positions. Dimensions to determine the correct
-        board size. 
-        """
+        # lists and dictionaries to store cars
         self.cars, self._cars_init = [], []
         self._rows_init, self._cols_init = {}, {}
 
-        """
-        Creation of empty board, empty board in string format and the playable gameboard.
-        """
+        # creation of the (empty) game board in differing formats
         self.empty_board = self.create_empty_gameboard(dimensions)
         self.gameboard, self.empty_board_string = self.create_gameboard(self.empty_board)
 
     def create_empty_gameboard(self, dimensions):
-        """
-        Creation of matrices containing ones and zeros in the correct dimensions. 
+        """Creation of an emtpy gameboard. 
+
+        Create matrices containing ones and zeros in the correct dimensions. 
         The ones matrix will provide a barrier around the board, needed to prevent out of range
         errors. The zeros will form the empty board.
         """
@@ -58,37 +39,33 @@ class Board():
         zeros_matrix = np.zeros((dimensions, dimensions))
         ones_matrix[1:dimensions + 1, 1:dimensions + 1] = zeros_matrix
         
-        """
-        Set one of the ones in the barrier to a two. This is needed to define the exit, 
-        which the red car should reach. 
-        """
+        # replace one of the one's in the barrier with a two to indicate exit
         col_out = dimensions + 1
         row_out = (dimensions + 1) // 2
         ones_matrix[row_out][col_out] = 2
         
+        # return the empty gameboard + barrier + exit
         return ones_matrix
         
 
     def create_gameboard(self, empty_board):
-        """
-        Take the empty gameboard and turn it into a playable gameboard
+        """Take the empty gameboard and turn it into usable gameboards.
+        
+        Cycle through the empty game board and transform each float into strings. 
         """
         empty_board_string = []
-        for i in empty_board: 
+        for row in empty_board: 
             row = []
-            for j in i: 
-                row.append(str(int(j)))
+            for cell in row: 
+                row.append(str(int(cell)))
             empty_board_string.append(row)
         gameboard = pickle.loads(pickle.dumps(empty_board_string))
         return gameboard, empty_board_string
 
     
-    """
-    Define function to load cars from .csv file into 
-    self.cars list. 
-    """
     def load_cars(self, Rushhour_df):
-        """
+        """Load cars from .csv file. 
+
         Cylce through the rows in the .csv file. 
         Extract the carname, orientation, row, column and lenght.
         Add carname and the corresponding starting row & column 
@@ -101,24 +78,22 @@ class Board():
             car_row = Rushhour_df.loc[index, "row"]
             car_length = Rushhour_df.loc[index, "length"]
 
+            # keep track of initial rows and columns
             self._cols_init[car_name] = car_column
             self._rows_init[car_name] = car_row
             
-            """
-            Use the Car() class to define the car and append it to 
-            self.cars list. 
-            """
+            # use Car() class to define cars and store them in the correct lists
             car = Car(car_name, car_orientation, car_column, car_row, car_length)
             car_init = Car(car_name, car_orientation, car_column, car_row, car_length)
             self.cars.append(car)
-        
             self._cars_init.append(car_init)
 
     """
     Define function to place cars from the self.cars list onto the gameboard. 
     """
     def place_car(self, cars):
-        """
+        """Place cars from list onto the gameboard. 
+
         First clear the previous game board. Cycle through the cars in self.cars 
         list and check their current rows, collumns, orientation and lenght. 
         Replace the zeroes in the empty gameboard with the car name. 
@@ -126,17 +101,18 @@ class Board():
         self.gameboard = pickle.loads(pickle.dumps(self.empty_board_string))
         for car in cars: 
             if car._orientation == "H":
-                for index in range(0, car._length):
-                    self.gameboard[car.row][car.column + index] = car._name
+                for length in range(0, car._length):
+                    # replace the strings in the empty gameboard with the car name
+                    self.gameboard[car.row][car.column + length] = car._name
             else:
-                for index in range(0, car._length):
-                    self.gameboard[car.row + index][car.column] = car._name.lower()
+                for length in range(0, car._length):
+                    # replace the strings in the empty gameboard with the car name
+                    self.gameboard[car.row + length][car.column] = car._name.lower()
 
-    """
-    Define function to move cars to the left.
-    """
+
     def moveCarLeft(self, carname):
-        """
+        """Move car left. 
+
         Cycle through the cars in self.cars list. Check orientation. 
         Check if the cell left of the car is empty. If so, move the car
         one column to the left. 
@@ -150,11 +126,9 @@ class Board():
                         self.movecounter += 1
                         return True
 
-    """
-    Define function to move cars to the right.
-    """
     def moveCarRight (self, carname):
-        """
+        """Move car right. 
+
         Cycle through the cars in self.cars list. Check orientation. 
         Check if the cell right of the car is empty. If so, move the car
         one column to the right. 
@@ -167,10 +141,8 @@ class Board():
                         self.place_car(self.cars)
                         self.movecounter += 1
                         return True
-                    """
-                    If the car willing to move is X (Red car), check if the cell on the right
-                    equals 2,, meaning the exit has been reached. 
-                    """
+                    # if the car to move is the red car, check if it reaches the exit with this move, 
+                    # if so end the game
                     if car._name == 'X':
                         if self.gameboard[car.row][car.column + car._length] == "2":
                             self.stop = timeit.default_timer()
@@ -179,11 +151,10 @@ class Board():
                             self.running = False
                             return True
     
-    """
-    Define function to move cars up.
-    """
+
     def moveCarUp (self, carname):
-        """
+        """Move car up. 
+
         Cycle through the cars in self.cars list. Check orientation. 
         Check if the cell above the car is empty. If so, move the car
         one row up. 
@@ -197,11 +168,10 @@ class Board():
                         self.movecounter += 1
                         return True 
 
-    """
-    Define function to move cars down.
-    """
+
     def moveCarDown(self, carname):
-        """
+        """Move car down. 
+
         Cycle through the cars in self.cars list. Check orientation. 
         Check if the cell below the car is empty. If so, move the car
         one row down.
@@ -215,14 +185,11 @@ class Board():
                         self.movecounter += 1
                         return True
 
-                
-    """
-    Define function to print the current game board. 
-    """
+
     def printBoard(self):
-        """
-        Convert the current self.gameboard to a list of lists into 
-        a temp. variable called gameboard. Cycle through the rows and cells
+        """Print the game board in colour. 
+
+        Cycle through the rows and cells
         in the current gameboard. If the cell represents a barrier or empty 
         spot, print in gray. If it represents the exit or "X" (Red car), print 
         in red. Otherwise assign random colour to the car, place it in 
@@ -233,13 +200,17 @@ class Board():
         for row in gameboard: 
             for cell in row: 
                 if cell == "1" or cell == "0": 
+                    # if cell is barrier or empty print in gray
                     cprint(int(cell), 'grey', end = "  ")
                     screenshot += str(cell)
                 elif cell == "2" or cell =="X": 
+                    # if cell is exit or red car, print in red
                     cprint(cell, 'red', end = '  ')
                     screenshot += str(cell)
                 else: 
                     if cell not in self.colourdict: 
+                        # if the cell has not been seen yet, pick random available 
+                        # colour and assign it to this specific car. duplicates possible
                         self.colourdict[cell] = random.choice(self.colour_list)
                     cprint(cell, self.colourdict[cell], end = " " * int(2/len(cell)))
                     screenshot += str(cell)
@@ -247,11 +218,8 @@ class Board():
         return screenshot
 
 
-    """
-    Define function cycle through the board in similar way as self.printBoard
-    only without printing. 
-    """
     def noprintBoard(self):
+        """Cycle through board similar way as self.printBoard without printing"""
         screenshot = ""
         gameboard = self.gameboard
         for row in gameboard: 
@@ -266,25 +234,18 @@ class Board():
                     screenshot += str(cell)
         return screenshot
     
-    """
-    Define function to write the desired output file. 
-    """
+
     def writeOutput(self):
-        """
-        Create an output dictionary to store the carname 
-        and corresponding moves. 
-        """
+        """Write requested output to output file in .csv file format"""
+        
+        # create output dictionary
         output = {}
         output['car'] = 'move'
 
-        """
-        Cycle through the cars in self.cars list and compare their 
-        current row/column with the initial row/column. 
-        Determine the number of cells it has moved and convert 
-        their values to a .csv file.
-        """
+        # cycle through current position of cars and calculate distance travelled from initial position
         for car in self.cars:
             output[car._name] = (car.row - self._rows_init[car._name]) + (car.column - self._cols_init[car._name])
         
+        # write output file
         output_series = pd.Series(data=output)
         output_series.to_csv('output/output.csv', header=False)
